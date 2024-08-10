10cbb4874e85dbe2d51a58eb82cb2bfa75ee19a6
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2024-08-10 12:23:04 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,13 @@ public static class ModHelperData
     public const string Version = "1.6.2";
     public const string Name = "Cams Pack!";
     public const string Dependencies = "doombubbles/paths-plus-plus";
-    public const string Description = "Everything I made in a pack! (DM on discord if there's any promblems with this mod Discord: camthekirby)";
+    public const string Description = "Everything I made in a pack!" +
+        " Features:" +
+        " New Towers," +
+        " New Paths," +
+        " New Bloons, and" +
+        " New Gamemodes" +
+        " (DM on discord if there's any promblems with this mod Discord: camthekirby)";
 
     public const string RepoOwner = "CamtheKirby"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CamsPack"; // TODO add your repo name here, also in the download url in README.md
