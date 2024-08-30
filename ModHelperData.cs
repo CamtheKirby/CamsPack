3dfa21765454fbe4e560922f09dfc690be3c8536
Cam the Kirby <92703353+CamtheKirby@users.noreply.github.com>
2024-08-30 08:17:53 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,13 +6,45 @@ public static class ModHelperData
     public const string Version = "1.6.2";
     public const string Name = "Cams Pack!";
     public const string Dependencies = "doombubbles/paths-plus-plus";
-    public const string Description = "Everything I made in a pack!" +
-        " Features:" +
-        " New Towers," +
-        " New Paths," +
-        " New Bloons, and" +
-        " New Gamemodes" +
-        " (DM on discord if there's any problems with this mod Discord: camthekirby) Credit: Nintendo (The Kirby characters), jacknjellify (Firey), u/PawaMV (The Dark Monkey Idea not the paragon), MaxLevelIdot (MaxLevelIdot)";
+    public const string Description = """
+         Everything I made in a pack!
+         Features:
+         New Towers,
+         New Paths, 
+         New Bloons, and
+         New Gamemodes 
+         (DM on discord if there's any problems with this mod Discord: camthekirby)
+
+         Credit: Nintendo (The Kirby characters), jacknjellify (Firey), u/PawaMV (The Dark Monkey Idea not the paragon), MaxLevelIdot (MaxLevelIdot)
+
+         I made everything else
+
+         QNA:
+
+         Q: did you make this for/work for [insert youtuber]
+
+         A: no I make these for fun but it's good to see youtubers covering my mod
+
+         Q: Can you add [tower idea]
+
+         A: Maybe it depends on the idea
+
+         Q: Why are there so many 2D towers and why are the towers blurry
+
+         A: I don't know how to code in models or make them and they're blurry because I have to shrink them so the towers aren't big this might be changed in the future
+
+         Q: You didn't make [insert tower] idea
+
+         A: I did. I would have credited it if not
+
+         Q: Why do the 2D towers rotate weirdly
+
+         A: idk and I don't know how to fix that
+
+         Q: Why is the mod so bad
+
+         A: I tried my best I don't really know C# so the quality of my mod won't be as good as other modders
+         """;
 
     public const string RepoOwner = "CamtheKirby"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CamsPack"; // TODO add your repo name here, also in the download url in README.md
